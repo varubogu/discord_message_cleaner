eb02b1007139dcb8c3c77ec6503f88057d6adb36
varubogu <toyosuke@varubogu.com>
2025-06-18 10:45:06 +0000

    feat: メッセージの関数を修正 削除に失敗した場合のリクエストを低減する Fixes #8

--- src/discord_bot/utils/messages.py
+++ src/discord_bot/utils/messages.py
@@ -1,12 +1,33 @@
+import os
 import json
 import aiofiles
 from pydantic import BaseModel, RootModel
+
+from discord_bot.utils.failed_reason_code import FailedReasonCode
 
 class Message(BaseModel):
     """メッセージ"""
 
     ja: str
     en: str
+
+    def get_message(self, lang: str) -> str:
+        """
+        メッセージを取得する
+
+        Args:
+            lang: 言語
+
+        Returns:
+            メッセージ
+        """
+
+        if lang == "ja":
+            return self.ja
+        elif lang == "en":
+            return self.en
+        else:
+            raise ValueError(f"Invalid language: {lang}")
 
 
 class Messages(RootModel):
@@ -17,11 +38,69 @@ class Messages(RootModel):
     def __getitem__(self, key: str) -> Message:
         return self.root[key]
 
+    async def get_message_locale(self) -> str:
+        """
+        メッセージのロケールを取得する
+
+        Returns:
+            メッセージのロケール
+        """
+
+        return os.environ.get("MESSAGE_LANGUAGE", "en")
+
+    async def get_log_locale(self) -> str:
+        """
+        ログのロケールを取得する
+
+        Returns:
+            ログのロケール
+        """
+
+        return os.environ.get("LOG_LANGUAGE", "en")
+
+    async def get_message(self, key: str, lang: str) -> str:
+        """
+        メッセージを取得する
+
+        Args:
+            key: メッセージのキー
+
+        Returns:
+            メッセージ
+        """
+
+        if key not in self.root:
+            raise ValueError(f"Message not found: {key}")
+
+        message = self[key]
+        try:
+            return message.get_message(lang)
+        except ValueError:
+            raise ValueError(f"Language not found: {lang}")
+
+    async def get_log_and_display_message(self, code: FailedReasonCode, display_locale: str) -> tuple[str, str]:
+        """
+        ログメッセージを出力する
+
+        Args:
+            code: エラーコード
+            display_locale: 表示言語
+
+        Returns:
+            ログメッセージと表示メッセージ
+        """
+
+        log_locale = await self.get_log_locale()
+        log_message = await self.get_message(code.value, log_locale)
+        display_message = await self.get_message(code.value, display_locale)
+
+        return log_message, display_message
+
 
 class SingletonMessages:
     """メッセージ一覧のシングルトン"""
 
-    _instance = None
+    _instance: Messages | None = None
 
     @classmethod
     async def get_instance(cls) -> Messages:
